ff5ac676bf48781cc8fa04493a270188f2656aab
taesu cho <taesuz@codeblue.co.kr>
2022-09-02 16:37:43 +0900

    - button 클래스 파라미터 타입 변경 - fvm 설정

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -1,28 +1,31 @@
-import 'package:early_care/component/class/button.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:early_care/component/button.dart';
+import 'package:early_care/component/color_info.dart';
 import 'package:flutter/material.dart';
 
-import '../component/class/color_info.dart';
-
-class Login extends StatefulWidget
-{
+class Login extends StatefulWidget {
   @override
   _Login createState() => _Login();
 }
 
-class _Login extends State<Login>
-{
+class _Login extends State<Login> {
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorInfo.white,
       body: SafeArea(
-        child: Column(
-          children: [
-            Button(10, 10, "first", 30, ColorInfo.mainColor, ColorInfo.white),
-            Button(30, 30, "first", 30, ColorInfo.mainColor, ColorInfo.white),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Button(
+                text: 'TEST',
+                backgroundColor: Colors.red,
+                onPressed: (){
+                  print('a');
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
